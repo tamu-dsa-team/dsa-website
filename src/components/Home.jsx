c6735f7e7c915b21a03b67df3ec8b0da1c644cc5
mustafatekin03 <mustafa21tekin@gmail.com>
2025-01-22 17:43:56 -0600

    Image added

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import "../styles/Home.css";
+import tamuImage from "../assets/tamu.jpg";
+
+function Home() {
+  return (
+    <div className="Main">
+      <img src={tamuImage} alt="Scenic Texas A&M Background Image" />
+    </div>
+  );
+}
+
+export default Home;
